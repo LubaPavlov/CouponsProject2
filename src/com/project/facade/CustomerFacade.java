85b0f6940858410bebd1c4000c8fe1fb826a7fee
LubaPavlov <lubavaga@gmail.com>
2017-05-15 18:41:44 +0300

    Update CustomerFacade.java

--- src/com/project/facade/CustomerFacade.java
+++ src/com/project/facade/CustomerFacade.java
@@ -1,5 +1,20 @@
 package com.project.facade;
 
-public class CustomerFacade {
+public class CustomerFacade implements CouponClientFacade {
+ 
+  private CustomerDAO customerDAO;
+  
+  public CustomerFacade() {
+  }
+  
+  	public CustomerFacade(CustomerDAO customerDAO) {
+		this.customerDAO = customerDAO;
+	}
+  
+  @Override
+	public CouponClientFacade login(String name, String password, ClientType clientType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
